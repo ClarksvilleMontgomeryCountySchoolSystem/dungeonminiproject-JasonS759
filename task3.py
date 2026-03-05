77f61c255872ebac12e7509e96cd84b2f8e9ac82
JasonS759 <jason.sakwe759@students.cmcss.net>
2026-03-05 09:13:41 -0600

    Commit Mwssage

---
+++ task3.py
@@ -0,0 +1,8 @@
+# Task 3 — The Guard
+guard_awake = False
+
+if not guard_awake:
+    outcome = "Shadow: You slip past the guard unnoticed."
+else:
+    outcome = "Doom: The guard spots you and raises the alarm."
+print(outcome)
